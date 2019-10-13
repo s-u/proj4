9da2f4e53c849b23389a6fbab6be78769b2b72a4
Jeroen Ooms <jeroenooms@gmail.com>
2019-10-13 14:03:32 +0200

    Windows: use proj 4.9.3 from rwinlib GDAL stack

---
+++ tools/winlibs.R
@@ -0,0 +1,9 @@
+# For details see: https://github.com/rwinlib/gdal2
+VERSION <- commandArgs(TRUE)
+if(!file.exists(sprintf("../windows/gdal2-%s/include/gdal/gdal.h", VERSION))){
+  if(getRversion() < "3.3.0") setInternet2()
+  download.file(sprintf("https://github.com/rwinlib/gdal2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+}
